d339c57df971222f9a9eb531083365118f27b600
Lanranyan <44810787+Lanranyan@users.noreply.github.com>
2019-05-17 08:47:28 -0700

    Add files via upload

--- the_game/scratch.py
+++ the_game/scratch.py
@@ -1,21 +1,57 @@
+# https://www.digitalocean.com/community/tutorials/how-to-install-pygame-and-create-a-template-for-developing-games-in-python-3
+# https://sivasantosh.wordpress.com/2012/07/16/pygame-template/
+# https://gist.github.com/MatthewJA/7544830
+# https://www.digitalocean.com/community/tutorials/understanding-tuples-in-python-3
+# https://medium.com/@angellom/writing-a-python-dungeon-game-part-i-47e35668f16b
+# https://rhettinger.wordpress.com/2011/05/26/super-considered-super/
+
 import pygame
 from pygame.locals import *
+from settings import *
+import os
+# evokes stuff like event.type w/o pygame. in front
 
+# Initialises pygame and creates a window
 pygame.init()
+# init = initialize - starts up all pygame modules
+# pygame.font.init()
+# https://www.pygame.org/docs/ref/font.html
 
-display_width = 800
-display_height = 600
+# i = pygame.init()
+# print(i)
+### Returns a tuple where x = successful pygame initializations and y = # of failures
 
-game_display = pygame.display.set_mode((display_width, display_height))
+# f = pygame.font.init()
+# print(f)
+### f returning None = the module isn't available within this particular environment
 
-pygame.display.update()
+# GAME SURFACE
 
-while True:
+
+
+game_display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+# (800, 600) is a tuple as argumuent to set_mode()
+pygame.display.set_caption('The Game')
+
+clock = pygame.time.Clock()
+
+# FUNCTIONS
+def event_handler():
     for event in pygame.event.get():
+        # print(event)
         if event.type == QUIT or (
-            event.type == KEYDOWN and (
-            event.key == K_ESCAPE or
-            event.key == K_q
-            )):
+                event.type == KEYDOWN and (
+                event.key == K_ESCAPE or
+                event.key == K_q
+        )):
             pygame.quit()
+            # uninitializes the pygame modules within pygame
             quit()
+            # exits the programs
+
+# GAME LOOP
+while True:
+    clock.tick(FPS)
+    event_handler()
+    pygame.display.update()
+    # updates portions of the screen, flip updates all
--- the_game/the_game.py
+++ the_game/the_game.py
@@ -1,56 +1,30 @@
-# https://www.digitalocean.com/community/tutorials/how-to-install-pygame-and-create-a-template-for-developing-games-in-python-3
-# https://sivasantosh.wordpress.com/2012/07/16/pygame-template/
-# https://gist.github.com/MatthewJA/7544830
-# https://www.digitalocean.com/community/tutorials/understanding-tuples-in-python-3
-# https://medium.com/@angellom/writing-a-python-dungeon-game-part-i-47e35668f16b
-
 import pygame
+import os
 from pygame.locals import *
 from settings import *
-import os
-# evokes stuff like event.type w/o pygame. in front
 
-# Initialises pygame and creates a window
-pygame.init()
-# init = initialize - starts up all pygame modules
-# pygame.font.init()
-# https://www.pygame.org/docs/ref/font.html
+class Game:
+    def __init__(self):
+        pygame.init()
+        game_screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+        pygame.display.set_caption('the_game')
+        self.clock = pygame.time.Clock()
 
-# i = pygame.init()
-# print(i)
-### Returns a tuple where x = successful pygame initializations and y = # of failures
-
-# f = pygame.font.init()
-# print(f)
-### f returning None = the module isn't available within this particular environment
-
-# GAME SURFACE
-
-
-
-game_display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
-# (800, 600) is a tuple as argumuent to set_mode()
-pygame.display.set_caption('The Game')
-
-clock = pygame.time.Clock()
-
-# FUNCTIONS
-def event_handler():
-    for event in pygame.event.get():
-        # print(event)
-        if event.type == QUIT or (
+    def exit_game(self):
+        for event in pygame.event.get():
+            if event.type == QUIT or (
                 event.type == KEYDOWN and (
                 event.key == K_ESCAPE or
                 event.key == K_q
-        )):
-            pygame.quit()
-            # uninitializes the pygame modules within pygame
-            quit()
-            # exits the program
+                )):
+                    pygame.quit()
+                    quit()
 
-# GAME LOOP
+class Player:
+    def __init__(self):
+        self.player = pygame.draw.rect(game_screen, WHITE, Rect)
+        self.update()
+
 while True:
-    clock.tick(FPS)
-    event_handler()
+    Game.clock.tick(FPS)
     pygame.display.update()
-    # updates portions of the screen, flip updates all
